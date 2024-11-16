c0b2a06d4f60ece61d796599824253c815005704
enver tohan <tohantr90@protonmail.com>
2024-11-16 21:47:37 +0300

    start.py

--- start.py
+++ start.py
@@ -23,20 +23,20 @@ def check_password():
             password_file_path = get_password_file_path()
 
             if not os.path.exists(password_file_path):
-                return jsonify({"message": "Şifre dosyası bulunamadı.", "found": False})
+                return jsonify({"message": "Password file not found.", "found": False})
 
             with open(password_file_path, "r") as file:
                 passwords = file.read().splitlines()
 
             if password in passwords:
-                return jsonify({"message": "Şifre bulundu!", "found": True})
+                return jsonify({"message": "Password found!", "found": True})
             else:
-                return jsonify({"message": "Şifre bulunamadı.", "found": False})
+                return jsonify({"message": "Password not found.", "found": False})
 
-        return jsonify({"message": "Lütfen bir şifre girin.", "found": False})
+        return jsonify({"message": "Please enter a password.", "found": False})
 
     except Exception as e:
-        return jsonify({"message": f"Bir hata oluştu: {str(e)}", "found": False})
+        return jsonify({"message": f"An error occurred: {str(e)}", "found": False})
 
 def open_browser():
     webbrowser.open("http://127.0.0.1:5005/")
